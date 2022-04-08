38d1b3ef81e47c96e6c7dfd7897b03601a4ec434
Sonny Vesali <79886185+sovesali@users.noreply.github.com>
2022-04-08 15:07:52 -0500

    image import fix

--- src/components/smartIcons/DAONationHeader/index.tsx
+++ src/components/smartIcons/DAONationHeader/index.tsx
@@ -1,14 +1,14 @@
-import Image from "next/Image";
+import Image from 'next/image';
 
 export default function DAONationHeader() {
-  const icon = require("./dao-nation-header.svg") as string;
+  const icon = require('./dao-nation-header.svg') as string;
   return (
     <Image
-      height="100px"
-      width="350px"
+      height='100px'
+      width='350px'
       style={{ margin: 0 }}
       src={icon}
-      alt="DAO Nation Header"
+      alt='DAO Nation Header'
     ></Image>
   );
 }
